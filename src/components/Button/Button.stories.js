376c3f6902556d8bd8235dd0bf7ffb79782cc419
Akio Satoh <devtunnel@gmail.com>
2020-01-14 16:57:59 -0800

    Added material-ui support

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import Button from '@material-ui/core/Button';
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => <Button variant='contained' color='default' onClick={action('clicked')}>Hello Button</Button>;
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button variant='contained' color='default' onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
